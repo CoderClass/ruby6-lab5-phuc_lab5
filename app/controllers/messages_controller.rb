f9778d16e02c0d9c139155d5afa8eb3ce9fe10db
Duy Phuc <phuc.tran@yomi.studio>
2017-07-12 20:10:07 +0700

    Implement create message in messagecontroller and add form_for message in messages view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,18 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_back(fallback_location: messages_path)
+    else
+      flash[:error] = 'Errors: #{@message.errors.full_messages.to_sentence}'
+      redirect_back(fallback_location: messages_path)
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
